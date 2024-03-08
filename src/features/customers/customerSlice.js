44e7292ecd34c9c246bb810a2434164e7efcad60
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-03-08 18:14:22 +0100

    updated: added redux-thunk

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -34,7 +34,7 @@ export function createCustomer(fullName, natioonalID) {
 
 export function updateName(fullName) {
   return {
-    type: "account/updateName",
+    type: "customer/updateName",
     payload: fullName,
   };
 }
